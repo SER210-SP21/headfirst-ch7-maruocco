8c803d379935445f08e9a6d16608d4d00946bd25
Michael Ruocco <michael.ruocco@quinnipiac.edu>
2023-02-16 08:28:05 -0500

    Chapter 6 demo

--- app/src/main/java/com/hfad/secretmessage/EncryptFragment.kt
+++ app/src/main/java/com/hfad/secretmessage/EncryptFragment.kt
@@ -5,8 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.findNavController
+import android.widget.TextView
 
 class EncryptFragment : Fragment() {
 
@@ -14,12 +13,10 @@ class EncryptFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_message, container, false)
-        val nextButton = view.findViewById<Button>(R.id.next)
-
-        nextButton.setOnClickListener{
-            view.findNavController().navigate(R.id.action_messageFragment_to_encryptFragment)
-        }
+        val view = inflater.inflate(R.layout.fragment_encrypt, container, false)
+        val message = EncryptFragmentArgs.fromBundle(requireArguments()).message
+        val encryptedView = view.findViewById<TextView>(R.id.encrypted_message)
+        encryptedView.text = message.reversed()
         return view
     }
 }
